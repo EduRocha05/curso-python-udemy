5a6f72b46b60ede9ecdef08cfc491c6fc37f56c4
Eduardo Rocha <edu_rocha05@hotmail.com>
2023-01-19 16:57:53 -0300

    Aula Tuplas

--- listas.py
+++ listas.py
@@ -142,17 +142,6 @@ for elemento in lista2:
     print(elemento)
     soma += elemento
 print(soma)
-'''
-
-type([])
-
-lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
-lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
-lista3 = []
-lista4 = list(range(11))
-lista5 = list('Geek University')
-
-
 
 #ex02 - utilizando while
 carrinho = []
@@ -161,7 +150,164 @@ while produto != 'sair':
     print("Adicione um produto na lista ou digite 'sair' ")
     produto = input()
     if produto != 'sair':
-        carrinho.append()
+        carrinho.append(produto)
 
 for produto in carrinho:
     print(produto)
+
+#utilizando variaveis em listas
+numeros = [1, 2, 3, 4, 5]
+
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+numeros = [num1, num2, num3, num4, num5]
+
+#fazemos acesso ao elementos de forma indexada
+cores = ['verde', 'amarelo', 'azul', 'branco']
+print(cores[0])
+print(cores[1])
+print(cores[2])
+print(cores[3])
+print()
+#fazer acesso ao elementos de forma indexada inversa
+print(cores[-1])
+print(cores[-2])
+print(cores[-3])
+print(cores[-4])
+
+for cor in cores:
+    print(cor)
+
+print()
+indice = 0
+while indice < len(cores):
+    print(cores[indice])
+    indice += 1
+
+
+#gerar indice em um for
+for indice, cor in enumerate(cores):
+    print(indice, cor)
+
+#listas aceitam valores repetidos
+lista = []
+lista.append(42)
+lista.append(42)
+lista.append(33)
+lista.append(33)
+lista.append(42)
+
+print(lista)
+
+#outros metodos
+#encotrar o indice de um elemento na lista
+
+numeros = [5, 6, 7, 5, 8, 9, 10]
+
+#em qual indice esta o valor 6?
+print(numeros.index(6))
+#em qual indice esta o valor 9?
+print(numeros.index(9))
+
+#caso tenha esse elemento na lista sera apresentado erro
+#print(numeros.index(19))
+
+#numeros iguais retorna o indice do primeiro elemento encontrado
+print(numeros.index(5))
+
+#podemos fazer busca dentro de um range,ou seja, qual indice começar a buscar
+print(numeros.index(5, 2)) #buscar 5 apartir do indice 2
+#print(numeros.index(5, 4)) #buscar 5 apartir do indice 4 - caso tenha esse elemento na lista sera apresentado erro
+
+#podemos fazer busca dentro de um range, inicio/fim
+print(numeros.index(8, 3, 6)) #buscar 5 a partir do indice 3 ate o 6
+
+
+#revisão slicing
+#lista[inicio:fim:passo]
+#range(inicio:fim:passo)
+
+#trabalhando com slice de lista com o parametro inicio
+lista = [1, 2, 3, 4]
+print(lista[1:]) #iniciando no indice 1 e pegando todos os elementos restantes
+
+#trabalhando com slice de lista com o parametro fim
+print(lista[:2]) #pegando todos os elementos do inicio e finalizando no indice 2
+
+#trabalhando com slice de lista com o parametro passo
+print(lista[1::2]) #começa do indice 1 ate o fim e pulando de 2 em 2
+print(lista[::2]) #pegando os elementos do inicio ate o fim e pulando de 2 em 2
+
+#invertendo valores em uma lista
+nomes = ['Geek', 'University']
+nomes[0], nomes[1] = nomes[1], nomes[0]
+print(nomes)
+nomes = ['Geek', 'University']
+nomes.reverse()
+print(nomes)
+
+#soma, valor maximo, valor minimo, tamanho
+#*se os valores forem todos inteiros ou reais
+
+lista = [1, 2, 3, 4, 5, 6]
+print(sum(lista))
+print(max(lista))
+print(min(lista))
+print(len(lista))
+
+#transformar uma lista em tupla
+lista = [1, 2, 3, 4, 5, 6]
+print(lista)
+print(type(lista))
+
+tupla = tuple(lista)
+print(tupla)
+print(type(tupla))
+
+#desempacotamento de listas
+listas = [1, 2, 3]
+
+num1, num2, num3 = listas
+
+print(num1)
+print(num2)
+print(num3)
+
+#copiando uma lista para outra(shallow copy e deep copy)
+#forma 1 - deep copy
+
+lista = [1, 2, 3]
+print(lista)
+
+nova = lista.copy()
+print(nova)
+nova.append(4)
+
+print(lista)
+print(nova)
+#ao utilizar .copy copiamos os dados da lista para uma nova lista mas elas ficaram totalmente independentes
+#modificando uma lista não altera a outra
+#chamado de deep copy (copia profunda)
+
+#forma 2 - shallow copy
+
+lista = [1, 2, 3]
+print(lista)
+
+nova = lista
+print(nova)
+nova.append(4)
+
+print(lista)
+print(nova)
+#ao utilizar a copia via atribuição e copiamos os dados para a nova lista mas apos modificar uma lista
+#essa modificação se refletiu em ambas as listas
+#chamado de shallow copy
+'''
+
+
+
+
---
+++ tuplas.py
@@ -0,0 +1,138 @@
+'''
+tuplas (tuple)
+
+são bastantes parecidas com listas
+existem duas diferenças
+1 - as tuplas são representadas por parenteses ()
+2 - as tuplas são imutaveis, isso significa que ao criar uma tupla ela não muda
+toda operação gera uma nova tupla
+
+print(type(()))
+
+#OBS 1: as tuplas são representadas por (), mas veja:
+tupla1 = (1, 2, 3, 4, 5, 6)
+print(tupla1)
+print(type(tupla1))
+print()
+tupla2 = 1, 2, 3, 4, 5, 6
+print(tupla2)
+print(type(tupla2))
+print()
+#OBS 2: as tuplas com 1 elemento:
+tupla3 = (4) #não é uma tupla!
+print(tupla3)
+print(type(tupla3))
+print()
+tupla4 = (4,) #é uma tupla!
+print(tupla4)
+print(type(tupla4))
+#CONCLUSÃO AS TUPLAS SÃO DEFINIDAS PELA VIRGULA E NÃO PELO PARENTESES
+
+#podemos gerar uma tupla dinamicamente com range(inicio, fim, passo)
+tupla = tuple(range(11))
+print(tupla)
+
+#desempacotamento de tupla
+tupla = ('Geek University', 'Programação em Python: Essencial')
+
+escola, curso = tupla
+
+print(escola)
+print(curso)
+#gera erro se colocarmos um numero diferente de elementos para desempacotar
+
+#OBS adição e  remoção de elementos nas tuplas não existem dado o fato das tuplas serem imutaveis
+
+#soma, valor max, valor min, tamanho
+#se valores forem inteiros ou reais
+
+tupla = (1, 2, 3, 4, 5, 6)
+print(sum(tupla))
+print(max(tupla))
+print(min(tupla))
+print(len(tupla))
+
+#concatenação de tuplas
+tupla1 = (1, 2, 3)
+print(tupla1)
+tupla2 = (4, 5, 6)
+print(tupla2)
+
+print(tupla1+tupla2)
+print(tupla1)
+print(tupla2)
+
+tupla3 = tupla1+tupla2
+print(tupla3)
+
+tupla1 = tupla1 + tupla2 #podemos sobscreever seus valores
+print(tupla1)
+
+#verificar se o elemento esta na tupla
+tupla1 = (1, 2, 3)
+print(3 in tupla1)
+
+#iterando sobre um tupla
+tupla1 = (1, 2, 3)
+for n in tupla1:
+    print(n)
+
+for indice, valor in enumerate(tupla1):
+    print(indice, valor)
+
+#contando elementos dentro de uma tupla
+tupla1 = ('a', 'b', 'c', 'd', 'a', 'b')
+print(tupla1.count('a'))
+
+escola = tuple('Geek University')
+print(escola)
+print(escola.count('e'))
+
+#dicas na utilização de tuplas
+#devemos utilizar tuplas SEMPRE que não precisarmos modificar os dados contidos em um coleção
+#exemplo1
+meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro',
+         'Novembro', 'Dezembro')
+print(meses)
+#acesso a elementos de uma tupla tambem é semelhante a de uma lista
+print(meses[5])
+#iterar com while
+
+i = 0
+while i < len(meses):
+    print(meses[i])
+    i+=1
+
+#verificamos em qual elemento esta na tupla
+print(meses.index('Junho'))
+
+#caso elemento não existe dara value erro
+
+#slicing
+
+#tupla[inicio:fim:passo]
+print(meses[0:])
+print(meses[5:])
+print(meses[5:9])
+
+#por que utilizar tuplas?
+
+# Tuplas são mais rapidas do que listas
+# Tuplas deixam seu codigo mais seguro, isso pq trabalhar com elementos imutavel traz segurança para o codigo
+
+#copiando uma tupla para outra
+
+tupla = (1, 2, 3)
+print(tupla)
+
+nova = tupla  #na tupla não temos problema de shallow copy
+
+outra = (4, 5, 6)
+
+nova += outra
+
+print(nova)
+print(tupla)
+'''
+
+
