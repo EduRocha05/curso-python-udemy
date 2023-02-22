e5c2a793a8986dcce1cc0bb82400680574fd7e84
Eduardo Rocha <edu_rocha05@hotmail.com>
2023-02-22 19:21:48 -0300

    Comprehensions - list, set, dict

---
+++ listas_aninhadas.py
@@ -0,0 +1,44 @@
+"""
+Listas aninhadas
+
+em algumas linguagens possuem uma estrutura de dados chamadas de arrays
+ - unidimensionais (arrays/vetores)
+ - Multidimensionais (matrizes)
+
+ Em python nos temos listas
+
+ # Exemplos
+
+listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  # Matriz 3x3
+print(listas)
+
+# Como fazemos para acessar os dados?
+
+print(listas[0][1])  # 2
+print(listas[2][1])  # 8
+
+# Iterando com loops em uma lista aninhada
+
+for lista in listas:
+    for num in lista:
+        print(num)
+
+# List Comprehension
+[[print(valor) for valor in lista] for lista in listas]
+
+
+
+"""
+
+# Exemplos - Gerando um tabuleiro matriz 3x3
+
+tabuleiro = [[numero for numero in range(1, 4)] for valor in range(1, 4)]
+print(tabuleiro)
+
+# Gerando jogadas para o jogo da velha
+velha = [['X' if numero % 2 == 0 else 'O' for numero in range(1, 4)] for valor in range(1, 4)]
+print(velha)
+
+# Gerando vaolres iniciais
+
+print([['*' for i in range(1, 4)] for j in range(1, 4)])
---
+++ set_comprehensions.py
@@ -0,0 +1,25 @@
+"""
+Set Comprehensions
+
+lista = [1, 2, 3, 4]
+conjunto = {1, 2, 3, 4}
+
+# Exemplos 1
+
+numeros = {num for num in range(1, 7)}
+print(numeros)
+
+# Exemplos 2
+
+numeros = {x ** 2 for x in range(10)}
+print(numeros)
+
+
+"""
+
+# Exemplo 3
+
+letras = {letra for letra in 'Geek University'}
+print(letras)
+
+# Set não possui ordenação ou repetição de valor
